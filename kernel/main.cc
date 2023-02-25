2d37eb1ac2327648fcfe31427b187eb56aa0e272
Asahi Takenouchi <asahi.taken@gmail.com>
2023-02-25 14:10:33 +0900

    feat: finish day05f

--- kernel/main.cc
+++ kernel/main.cc
@@ -15,16 +15,36 @@ void operator delete(void* obj) noexcept {}
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
 
+char console_buf[sizeof(Console)];
+Console* console;
+
+// NOLINTNEXTLINE(cert-dcl50-cpp)
+int printk(const char* format, ...) {
+  va_list ap;
+  int result;
+  char s[1024];
+
+  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-vararg,hicpp-vararg,cppcoreguidelines-pro-bounds-array-to-pointer-decay,hicpp-no-array-decay)
+  va_start(ap, format);
+  // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-array-to-pointer-decay,hicpp-no-array-decay)
+  result = vsprintf(static_cast<char*>(s), format, ap);
+  // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-array-to-pointer-decay,hicpp-no-array-decay)
+  va_end(ap);
+
+  console->PutString(static_cast<const char*>(s));
+  return result;
+}
+
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
   switch (frame_buffer_config.pixel_format) {
     case kPixelRGBResv8BitPerColor:
-      // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-array-to-pointer-decay,hicpp-no-array-decay)
-      pixel_writer = new (pixel_writer_buf)
+      // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+      pixel_writer = new (reinterpret_cast<PixelWriter*>(pixel_writer_buf))
           RGBResv8BitPerColorPixelWriter{frame_buffer_config};
       break;
     case kPixelBGRResv8BitPerColor:
-      // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-array-to-pointer-decay,hicpp-no-array-decay)
-      pixel_writer = new (pixel_writer_buf)
+      // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+      pixel_writer = new (reinterpret_cast<PixelWriter*>(pixel_writer_buf))
           BGRResv8BitPerColorPixelWriter{frame_buffer_config};
       break;
   }
@@ -39,13 +59,13 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
    * Show console
    */
 
-  Console console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
+  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast,cppcoreguidelines-owning-memory)
+  console = new (reinterpret_cast<Console*>(console_buf))
+      Console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
 
-  char buf[128];
   for (int i = 0; i < 27; ++i) {
     // NOLINTNEXTLINE(cppcoreguidelines-pro-type-vararg,hicpp-vararg)
-    sprintf(static_cast<char*>(buf), "line %d\n", i);
-    console.PutString(static_cast<const char*>(buf));
+    printk("printk; %d\n", i);
   }
 
   /**
